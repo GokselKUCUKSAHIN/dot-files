56d9ce098105b63d44876ae823aa48eb80f24f94
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2021-03-29 04:25:35 +0300

    Update .my_custom_commands.sh

--- macOS/.my_custom_commands.sh
+++ macOS/.my_custom_commands.sh
@@ -10,6 +10,51 @@ function say_my_name()
 	echo 'Master JellyBeanci'
 }
 
+function openBishamon()
+{
+	ssh -i "~/.key.pem" ubuntu@bishamon.ml
+}
+
+function openBishamonWithKey()
+{
+	key=/Volumes/FDD/secret.key
+	if [ -f "$key" ]; then
+		openBishamon
+	else
+		clear
+		node ~/.custom_repos/floppy.js
+		echo -n "Please Insert your key! and Press y to Continue ";
+		if asksure; then
+  		echo "Okay, looking for the Key file..."
+			while ! [ -f "$key" ];
+			do
+        echo -n "# "
+        sleep 1
+      done
+			openBishamonWithKey
+		else
+  		echo "Good Bye..."
+		fi
+  fi
+}
+
+asksure() {
+while read -r -n 1 -s answer; do
+  if [[ $answer = [YyNn] ]]; then
+    [[ $answer = [Yy] ]] && retval=0
+    [[ $answer = [Nn] ]] && retval=1
+    break
+	else
+		retval=1
+	  break
+  fi
+done
+
+echo # just a final linefeed, optics...
+
+return $retval
+}
+
 function connectPi()
 {
 	#ssh pi@192.168.1.${1}
@@ -35,4 +80,9 @@ function mkcd()
 function cl()
 {
 	cd $1 && ls
+}
+
+function juan()
+{
+	node ~/.custom_repos/juan.js
 }
