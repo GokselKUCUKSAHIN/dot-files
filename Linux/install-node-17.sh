0987d97c683df99fc091d5163292f5b0b5f4868b
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2021-10-28 08:56:17 +0300

    Node v17

--- Linux/install-node-17.sh
+++ Linux/install-node-17.sh
@@ -2,4 +2,4 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 command -v nvm
-nvm install 16.0.0
+nvm install 17.0.0
